5cbbfa27ce8abdc8c3984fabe78f6bcfa883a7f7
Adrineel-Saha <adrineelsaha@gmail.com>
2026-06-04 18:06:14 +0530

    Auth Header Changes for OrderService

--- src/main/java/com/cognizant/orderservice/main/OrderServiceApplication.java
+++ src/main/java/com/cognizant/orderservice/main/OrderServiceApplication.java
@@ -2,6 +2,11 @@ package com.cognizant.orderservice.main;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +15,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.util.List;
 
 @SpringBootApplication(scanBasePackages = "com.cognizant.orderservice.*")
 @EnableJpaRepositories(basePackages = "com.cognizant.orderservice.repositories")
@@ -33,5 +40,23 @@ public class OrderServiceApplication {
 	public ModelMapper modelMapper()
 	{
 		return new ModelMapper();
+	}
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+				.servers(List.of(
+						new Server().url("http://localhost:9191").description("API Gateway")
+				))
+				.addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+				.components(new Components()
+						.addSecuritySchemes("bearerAuth",
+								new SecurityScheme()
+										.name("bearerAuth")
+										.type(SecurityScheme.Type.HTTP)
+										.scheme("bearer")
+										.bearerFormat("JWT")
+						)
+				);
 	}
 }
